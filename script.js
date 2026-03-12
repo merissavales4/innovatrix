c64c089554c0b61e55754cd18c2ab6c9e9e5946d
Merissa Vales <merissavales4a@gmail.com>
2026-03-12 22:42:58 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function addTask(){
+
+let taskInput = document.getElementById("taskInput");
+let taskText = taskInput.value;
+
+if(taskText === ""){
+alert("Enter a task");
+return;
+}
+
+let li = document.createElement("li");
+
+li.innerHTML = taskText + 
+"<button onclick='this.parentElement.remove()'>Delete</button>";
+
+document.getElementById("taskList").appendChild(li);
+
+taskInput.value = "";
+}
